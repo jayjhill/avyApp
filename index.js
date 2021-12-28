89b1e08d043d244e8551fe2e30de53adf2fcf444
jayjhill <hill.jay@gmail.com>
2021-12-27 19:04:05 -0700

    two files

--- index.js
+++ index.js
@@ -1,45 +1,71 @@
-        avyGridTop = [];
-        avyGridMid = [];
-        avyGridBottom = [];
-        
-        
-        //arrays with x,y data specific to the rose png
+avyGridTop = [];
+avyGridMid = [];
+avyGridBottom = [];
+
+
+const fs = require('fs')
+fs.readFile('./json/' + 'avyJson' + yyyymmdd() + '.json', 'utf8', (err, jsonString) => {
+if (err) {
+console.log("File read failed:", err)
+return
+}
+try {
+const report = JSON.parse(jsonString)
+console.log("Array:", report.overall_danger_rose) // => "Customer address is: Infinity Loop Drive"
+} catch(err) {
+console.log('Error parsing JSON string:', err)
+}
+})
+
+function yyyymmdd() {
+var now = new Date();
+var y = now.getFullYear();
+var m = now.getMonth() + 1;
+var d = now.getDate();
+var mm = m < 10 ? '0' + m : m;
+var dd = d < 10 ? '0' + d : d;
+return '' + y + mm + dd;
+}
+
+
+
+//arrays with x,y data specific to the rose png
 var topArray = [
-    {x: 198, y: 132},
-    {x: 219, y: 138},
-    {x: 233, y: 157},
-    {x: 225, y: 179},
-    {x: 201, y: 187},
-    {x: 177, y: 176},
-    {x: 163, y: 156},
-    {x: 178, y: 138},
-    {x: 204, y: 104},
-   {x: 254, y: 120},
-   {x: 277, y: 163},
-   {x: 252, y: 213},
-   {x: 195, y: 229},
-   {x: 149, y: 208},
-   {x: 128, y: 165},
-   {x: 149, y: 122},
-   {x: 200, y: 131},
-   {x: 223, y: 137},
-   {x: 231, y: 156},
-   {x: 221, y: 177},
-   {x: 201, y: 186},
-   {x: 176, y: 178},
-   {x: 168, y: 158},
-   {x: 177, y: 138}
+{x: 198, y: 132},
+{x: 219, y: 138},
+{x: 233, y: 157},
+{x: 225, y: 179},
+{x: 201, y: 187},
+{x: 177, y: 176},
+{x: 163, y: 156},
+{x: 178, y: 138},
+{x: 204, y: 104},
+{x: 254, y: 120},
+{x: 277, y: 163},
+{x: 252, y: 213},
+{x: 195, y: 229},
+{x: 149, y: 208},
+{x: 128, y: 165},
+{x: 149, y: 122},
+{x: 200, y: 69},
+{x: 290, y: 102},
+{x: 327, y: 175},
+{x: 286, y: 254},
+{x: 197, y: 284},
+{x: 112, y: 254},
+{x: 76, y: 175},
+{x: 115, y: 103}
 ];
 
 
 function yyyymmdd() {
-    var now = new Date();
-    var y = now.getFullYear();
-    var m = now.getMonth() + 1;
-    var d = now.getDate();
-    var mm = m < 10 ? '0' + m : m;
-    var dd = d < 10 ? '0' + d : d;
-    return '' + y + mm + dd;
+var now = new Date();
+var y = now.getFullYear();
+var m = now.getMonth() + 1;
+var d = now.getDate();
+var mm = m < 10 ? '0' + m : m;
+var dd = d < 10 ? '0' + d : d;
+return '' + y + mm + dd;
 }
 
 
@@ -51,39 +77,49 @@ console.log(img.src);
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 img.onload = function() {
-  ctx.drawImage(img, 0, 0);
-  img.style.display = 'none';
-    topArray.forEach(pickArray);
-    function pickArray(event) {
-  var x = event.x;
-  var y = event.y;
+ctx.drawImage(img, 0, 0);
+img.style.display = 'none';
+topArray.forEach(pickArray);
+function pickArray(event) {
+var x = event.x;
+var y = event.y;
 
-  var pixel = ctx.getImageData(x, y, 1, 1);
-  var data = pixel.data;
-  var r = data[0];
-  var g = data[1];
-  var b = data[2];
+var pixel = ctx.getImageData(x, y, 1, 1);
+var data = pixel.data;
+var r = data[0];
+var g = data[1];
+var b = data[2];
 
-  var rgb = {r, g, b};
-  avyGridTop.push(rgb);
+//var rgb = {r, g, b};
+var rgb = 'rgb(' + r + ', ' + g + ', ' + b + ')';
+avyGridTop.push(rgb);
 
 }
 }
-  
-  
-  mapboxgl.accessToken = 'pk.eyJ1IjoiamNoaWxsIiwiYSI6ImNrZGl0cGlpbzA4ZmEzMm8wZHdkYmJiNDMifQ.C941o-cDXISu58gsmm8sIw';
-  var map = new mapboxgl.Map({
-    container: 'map', // container id
-   style: 'mapbox://styles/mapbox-map-design/ckhqrf2tz0dt119ny6azh975y', // stylesheet location
-    center: [-111.651, 40.571], // starting position [lng, lat]
-    zoom: 15, // starting zoom
-    pitch: 50,
+
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiamNoaWxsIiwiYSI6ImNrZGl0cGlpbzA4ZmEzMm8wZHdkYmJiNDMifQ.C941o-cDXISu58gsmm8sIw';
+var map = new mapboxgl.Map({
+container: 'map', // container id
+style: 'mapbox://styles/mapbox-map-design/ckhqrf2tz0dt119ny6azh975y', // stylesheet location
+center: [-111.651, 40.621], // starting position [lng, lat]
+zoom: 12, // starting zoom
+pitch: 50,
 bearing: 0,
-  });
+});
 
-  map.on('load', function() {
-  
-  map.addSource('mapbox-dem', {
+map.addControl(
+new mapboxgl.GeolocateControl({
+positionOptions: {
+enableHighAccuracy: true
+},
+trackUserLocation: true
+})
+)
+
+map.on('load', function() {
+
+map.addSource('mapbox-dem', {
 'type': 'raster-dem',
 'url': 'mapbox://mapbox.mapbox-terrain-dem-v1',
 'tileSize': 512,
@@ -91,7 +127,9 @@ bearing: 0,
 });
 // add the DEM source as a terrain layer with exaggerated height
 map.setTerrain({ 'source': 'mapbox-dem', 'exaggeration': 1 });
- 
+
+console.log(avyGridTop);
+
 // add a sky layer that will show when the map is highly pitched
 map.addLayer({
 'id': 'sky',
@@ -104,84 +142,110 @@ map.addLayer({
 });
 
 var colorOne = 'rgb(255,0,0)';
-  
-    map.addLayer({
-      id: 'SLC_LessThan8000',
-      type: 'fill',
-      source: {
-        type: 'vector',
-        url: 'mapbox://jchill.a3iffyuy'
-      },
-      'source-layer': 'SLC_LessThan8000',
-      'paint': {
+
+map.addLayer({
+id: 'SLC_LessThan8000',
+type: 'fill',
+source: {
+type: 'vector',
+url: 'mapbox://jchill.a3iffyuy'
+},
+'source-layer': 'SLC_LessThan8000',
+'paint': {
 'fill-color': [
 'match',
 ['get', 'gridcode'],
 '0',
-'#fbb03b',
+avyGridTop[16],
 '1',
-'#223b53',
+avyGridTop[17],
 '2',
-'#e55e5e',
+avyGridTop[18],
 '3',
-'#3bb2d0',
-/* other */ '#ccc'
+avyGridTop[19],
+'4',
+avyGridTop[20],
+'5',
+avyGridTop[21],
+'6',
+avyGridTop[22],
+'7',
+avyGridTop[23],
+/* other */ '#ffffff'
 ],
-'fill-opacity': 0.8
+'fill-opacity': 0.7
 }
-    });
-    
- map.addLayer({
-      id: 'SLC_8000to9500',
-      type: 'fill',
-      source: {
-        type: 'vector',
-        url: 'mapbox://jchill.15vi4pm2'
-      },
-      'source-layer': 'SLC_8000to9500',
-      'paint': {
+});
+
+map.addLayer({
+id: 'SLC_8000to9500',
+type: 'fill',
+source: {
+type: 'vector',
+url: 'mapbox://jchill.15vi4pm2'
+},
+'source-layer': 'SLC_8000to9500',
+'paint': {
 'fill-color': [
 'match',
 ['get', 'gridcode'],
 '0',
-'#fbb03b',
+avyGridTop[8],
 '1',
-'#223b53',
+avyGridTop[9],
 '2',
-'#e55e5e',
+avyGridTop[10],
 '3',
-'#3bb2d0',
-/* other */ '#ccc'
+avyGridTop[11],
+'4',
+avyGridTop[12],
+'5',
+avyGridTop[13],
+'6',
+avyGridTop[14],
+'7',
+avyGridTop[15],
+/* other */ '#ffffff'
 ],
-'fill-opacity': 0.8
+'fill-opacity': 0.7
 }
-    });
-    
- map.addLayer({
-      id: 'SLC_GreaterThan9500',
-      type: 'fill',
-      source: {
-        type: 'vector',
-        url: 'mapbox://jchill.bvz2202i'
-      },
-      'source-layer': 'SLC_GreaterThan9500',
-      'paint': {
+});
+
+
+
+map.addLayer({
+id: 'SLC_GreaterThan9500',
+type: 'fill',
+source: {
+type: 'vector',
+url: 'mapbox://jchill.bvz2202i'
+},
+'source-layer': 'SLC_GreaterThan9500',
+'paint': {
 'fill-color': [
 'match',
 ['get', 'gridcode'],
 '0',
-'#fbb03b',
+avyGridTop[0],
 '1',
-'#223b53',
+avyGridTop[1],
 '2',
-'#e55e5e',
+avyGridTop[2],
 '3',
-'#3bb2d0',
-/* other */ '#ccc'
+avyGridTop[3],
+'4',
+avyGridTop[4],
+'5',
+avyGridTop[5],
+'6',
+avyGridTop[6],
+'7',
+avyGridTop[7],
+/* other */ '#ffffff'
 ],
-'fill-opacity': 0.8
+'fill-opacity': 0.7
 }
-    });
-    
-    
-  });
+});
+
+
+});
